7344b97c0a56884ca9354bb30590cf8df557ce18
Francis Eduardo Ribeiro <franedurib@gmail.com>
2017-02-27 11:47:14 -0300

    app básico funcionando

--- exemplo-labeling-app/pacotes_28309/pacotes_28309/control/AppControl.java
+++ exemplo-labeling-app/pacotes_28309/pacotes_28309/control/AppControl.java
@@ -1,5 +1,29 @@
 package pacotes_28309.control;
 
-public class AppControl {
+import java.awt.event.*;
+import pacotes_28309.view.*;
+
+/**
+ * Classe AppControl: Principal controler da aplicação.
+ * 
+ * @author Francis Ribeiro
+ */
+public class AppControl implements ActionListener {
+
+	private Tela appView;
+
+	/**
+	 * Construtor da classe. Exibe a aplicação.
+	 */
+	public AppControl() {
+		appView = new Tela(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Botão")) {
+			System.out.println("cliquei no Botão");
+		}
+	}
 
 }
