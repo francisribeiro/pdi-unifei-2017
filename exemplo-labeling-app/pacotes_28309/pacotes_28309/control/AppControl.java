052cd82d7ae34af8e8858b9d1847d1534d54cb05
Francis Eduardo Ribeiro <franedurib@gmail.com>
2017-02-27 21:11:22 -0300

    Carregamento de imagem

--- exemplo-labeling-app/pacotes_28309/pacotes_28309/control/AppControl.java
+++ exemplo-labeling-app/pacotes_28309/pacotes_28309/control/AppControl.java
@@ -1,6 +1,10 @@
 package pacotes_28309.control;
 
 import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
 import pacotes_28309.view.*;
 
 /**
@@ -11,6 +15,8 @@ import pacotes_28309.view.*;
 public class AppControl implements ActionListener {
 
 	private Tela appView;
+	private String path;
+	private BufferedImage img;
 
 	/**
 	 * Construtor da classe. Exibe a aplicação.
@@ -21,9 +27,66 @@ public class AppControl implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("Botão")) {
-			System.out.println("cliquei no Botão");
+		if (e.getActionCommand().equals("Abrir Imagem")) {
+			path = caminhoImagem();
+			img = carregarImagem(path);
+			appView.setImagemDireita(img);
 		}
+		
+		if (e.getActionCommand().equals("Rotular Imagem")) {
+
+		}
+	}
+
+	/**
+	 * Método para navegar pela estrutura de diretórios e carregar uma imagem.
+	 * 
+	 * @return nomeArqLido Nome do arquivo lido em uma String
+	 */
+	private String caminhoImagem() {
+		JFileChooser arquivo;
+		File diretorio, nomeArq = null;
+		String nomeArqLido = null;
+		int saida;
+
+		// Localizando o arquivo
+		arquivo = new JFileChooser();
+		diretorio = new File("..\\");
+		arquivo.setCurrentDirectory(diretorio);
+		arquivo.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		saida = arquivo.showOpenDialog(arquivo);
+
+		if (saida == JFileChooser.APPROVE_OPTION) {
+			// Fazendo a leitura do arquivo
+			nomeArq = arquivo.getSelectedFile();
+			nomeArqLido = nomeArq.toString();
+		}
+
+		return nomeArqLido;
+	}
+
+	/**
+	 * Método para carregar na mémória uma imagem.
+	 * 
+	 * @param path
+	 *            Caminho para a Imagem.
+	 * @return imagem BufferedImage contendo a imagem necessária.
+	 */
+	private BufferedImage carregarImagem(String path) {
+		BufferedImage imagem = null;
+		String msg;
+
+		try {
+			imagem = ImageIO.read(new File(path));
+		} catch (FileNotFoundException e) {
+			msg = "Arquivo não encontrado.";
+			JOptionPane.showMessageDialog(null, msg, "", JOptionPane.INFORMATION_MESSAGE);
+		} catch (IOException e) {
+			msg = "Erro no arquivo.";
+			JOptionPane.showMessageDialog(null, msg, "", JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		return imagem;
 	}
 
 }
--- exemplo-labeling-app/pacotes_28309/pacotes_28309/view/Tela.java
+++ exemplo-labeling-app/pacotes_28309/pacotes_28309/view/Tela.java
@@ -1,6 +1,7 @@
 package pacotes_28309.view;
 
 import java.awt.*;
+import java.awt.image.*;
 import javax.swing.*;
 import pacotes_28309.control.*;
 
@@ -10,7 +11,9 @@ import pacotes_28309.control.*;
  * @author Francis Ribeiro
  */
 public class Tela extends JFrame {
-	public JButton btnCarregarImagem;
+	private JButton btnCarregarImagem, btnRotularImagem;
+	private JPanel container, panelOne, panelTwo;
+	private JLabel lblImgDireita, lblImgEsquerda;
 
 	/**
 	 * Contrutor da classe. Define as configurações da janela principal da
@@ -22,7 +25,7 @@ public class Tela extends JFrame {
 	public Tela(AppControl appControl) {
 
 		// Setando as configurações da janela.
-		this.setTitle("App Chassi");
+		this.setTitle("Exemplo Labeling App");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setLayout(new BorderLayout());
@@ -30,6 +33,27 @@ public class Tela extends JFrame {
 
 		// Adicionando a barra de ferramentas ao JFrame.
 		toolBar(this, appControl);
+
+		// Painéis da aplicação
+		container = new JPanel();
+		panelOne = new JPanel();
+		panelTwo = new JPanel();
+
+		// Label contendo as imagens
+		lblImgDireita = new JLabel();
+		lblImgEsquerda = new JLabel();
+
+		// Setando as Imagens nos painéis
+		panelOne.add(lblImgDireita);
+		panelTwo.add(lblImgEsquerda);
+
+		// Ajustando os painíes ao painel de container
+		container.setLayout(new GridLayout(1, 2));
+		container.add(panelOne);
+		container.add(panelTwo);
+
+		// Adicionando o container ao frame.
+		this.add(container);
 
 		// Empacotando e exibindo a aplicação.
 		this.pack();
@@ -44,7 +68,7 @@ public class Tela extends JFrame {
 	 * @param appControl
 	 *            Controle principal da aplicação.
 	 */
-	protected void toolBar(JFrame frame, AppControl appControl) {
+	private void toolBar(JFrame frame, AppControl appControl) {
 		JToolBar toolBar;
 
 		// Cria a barra de ferramentas.
@@ -52,15 +76,41 @@ public class Tela extends JFrame {
 		toolBar.setRollover(true);
 
 		// Cria os botões.
-		btnCarregarImagem = new JButton("Botão");
+		btnCarregarImagem = new JButton("Abrir Imagem");
+		btnRotularImagem = new JButton("Rotular Imagem");
 
 		// Adiciona os listeners.
 		btnCarregarImagem.addActionListener(appControl);
-
+		btnRotularImagem.addActionListener(appControl);
+		
 		toolBar.add(btnCarregarImagem);
+		toolBar.addSeparator();
+		
+		toolBar.add(btnRotularImagem	);
 		toolBar.addSeparator();
 
 		// Adiciona toolbar ao JFrame.
 		frame.add(toolBar, BorderLayout.NORTH);
+	}
+
+	/**
+	 * Método que exibe uma imagem na metade direita da tela.
+	 * 
+	 * @param image
+	 *            BufferedImage contendo a imagem a ser transformada.
+	 */
+	public void setImagemDireita(BufferedImage image) {
+		this.lblImgDireita.setIcon(new ImageIcon(image));
+	}
+
+	
+	/**
+	 * Método que exibe uma imagem na metade esquerda da tela.
+	 * 
+	 * @param image
+	 *            BufferedImage contendo a imagem a transformada.
+	 */
+	public void setImagemEsquerda(BufferedImage image) {
+		this.lblImgEsquerda.setIcon(new ImageIcon(image));
 	}
 }
