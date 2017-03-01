2a76544eba5a0b98b6a4c0c7ddc8623e2042e0b5
Francis Eduardo Ribeiro <franedurib@gmail.com>
2017-03-01 11:20:05 -0300

    apagando arquivos

--- exemplo-labeling-app/pacotes_28309/pacotes_28309/control/AppControl.java
+++ exemplo-labeling-app/pacotes_28309/pacotes_28309/control/AppControl.java
@@ -15,7 +15,6 @@ import pacotes_28309.view.*;
 public class AppControl implements ActionListener {
 
 	private Tela appView;
-	private String path;
 	private BufferedImage img;
 
 	/**
@@ -28,65 +27,13 @@ public class AppControl implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("Abrir Imagem")) {
-			path = caminhoImagem();
-			img = carregarImagem(path);
+			img = appView.carregarImagem();
 			appView.setImagemDireita(img);
 		}
-		
+
 		if (e.getActionCommand().equals("Rotular Imagem")) {
-
+			appView.setImagemEsquerda(img);
 		}
-	}
-
-	/**
-	 * Método para navegar pela estrutura de diretórios e carregar uma imagem.
-	 * 
-	 * @return nomeArqLido Nome do arquivo lido em uma String
-	 */
-	private String caminhoImagem() {
-		JFileChooser arquivo;
-		File diretorio, nomeArq = null;
-		String nomeArqLido = null;
-		int saida;
-
-		// Localizando o arquivo
-		arquivo = new JFileChooser();
-		diretorio = new File("..\\");
-		arquivo.setCurrentDirectory(diretorio);
-		arquivo.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		saida = arquivo.showOpenDialog(arquivo);
-
-		if (saida == JFileChooser.APPROVE_OPTION) {
-			// Fazendo a leitura do arquivo
-			nomeArq = arquivo.getSelectedFile();
-			nomeArqLido = nomeArq.toString();
-		}
-
-		return nomeArqLido;
-	}
-
-	/**
-	 * Método para carregar na mémória uma imagem.
-	 * 
-	 * @param path
-	 *            Caminho para a Imagem.
-	 * @return imagem BufferedImage contendo a imagem necessária.
-	 */
-	private BufferedImage carregarImagem(String path) {
-		BufferedImage imagem = null;
-		String msg;
-
-		try {
-			imagem = ImageIO.read(new File(path));
-		} catch (FileNotFoundException e) {
-			msg = "Arquivo não encontrado.";
-			JOptionPane.showMessageDialog(null, msg, "", JOptionPane.INFORMATION_MESSAGE);
-		} catch (IOException e) {
-			msg = "Erro no arquivo.";
-			JOptionPane.showMessageDialog(null, msg, "", JOptionPane.INFORMATION_MESSAGE);
-		}
-
-		return imagem;
 	}
 
 }
--- exemplo-labeling-app/pacotes_28309/pacotes_28309/view/Tela.java
+++ exemplo-labeling-app/pacotes_28309/pacotes_28309/view/Tela.java
@@ -2,6 +2,11 @@ package pacotes_28309.view;
 
 import java.awt.*;
 import java.awt.image.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import pacotes_28309.control.*;
 
@@ -82,15 +87,66 @@ public class Tela extends JFrame {
 		// Adiciona os listeners.
 		btnCarregarImagem.addActionListener(appControl);
 		btnRotularImagem.addActionListener(appControl);
-		
+
+		// Colocando os botões na barra de ferramentas.
 		toolBar.add(btnCarregarImagem);
 		toolBar.addSeparator();
-		
-		toolBar.add(btnRotularImagem	);
+
+		toolBar.add(btnRotularImagem);
 		toolBar.addSeparator();
 
 		// Adiciona toolbar ao JFrame.
 		frame.add(toolBar, BorderLayout.NORTH);
+	}
+
+	/**
+	 * Método para navegar pela estrutura de diretórios e carregar uma imagem.
+	 * 
+	 * @return nomeArqLido Nome do arquivo lido em uma String
+	 */
+	private String caminhoImagem() {
+		JFileChooser arquivo;
+		File diretorio, nomeArq = null;
+		String nomeArqLido = null;
+		int saida;
+
+		// Localizando o arquivo
+		arquivo = new JFileChooser();
+		diretorio = new File("..\\");
+		arquivo.setCurrentDirectory(diretorio);
+		arquivo.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		saida = arquivo.showOpenDialog(arquivo);
+
+		if (saida == JFileChooser.APPROVE_OPTION) {
+			// Fazendo a leitura do arquivo
+			nomeArq = arquivo.getSelectedFile();
+			nomeArqLido = nomeArq.toString();
+		}
+
+		return nomeArqLido;
+	}
+
+	/**
+	 * Método para carregar na mémória uma imagem.
+	 * 
+	 * @return imagem BufferedImage contendo a imagem necessária.
+	 */
+	public BufferedImage carregarImagem() {
+		BufferedImage imagem = null;
+		String msg, path;
+
+		try {
+			path = caminhoImagem();// Localiza o caminho da imagem.
+			imagem = ImageIO.read(new File(path));// Carrega a imagem.
+		} catch (FileNotFoundException e) {
+			msg = "Arquivo não encontrado.";
+			JOptionPane.showMessageDialog(null, msg, "", JOptionPane.INFORMATION_MESSAGE);
+		} catch (IOException e) {
+			msg = "Erro no arquivo.";
+			JOptionPane.showMessageDialog(null, msg, "", JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		return imagem;
 	}
 
 	/**
@@ -103,12 +159,11 @@ public class Tela extends JFrame {
 		this.lblImgDireita.setIcon(new ImageIcon(image));
 	}
 
-	
 	/**
 	 * Método que exibe uma imagem na metade esquerda da tela.
 	 * 
 	 * @param image
-	 *            BufferedImage contendo a imagem a transformada.
+	 *            BufferedImage contendo a imagem transformada.
 	 */
 	public void setImagemEsquerda(BufferedImage image) {
 		this.lblImgEsquerda.setIcon(new ImageIcon(image));
