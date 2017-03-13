22ffa1f84248101841a0d359391d26a39c6c0385
Francis Eduardo Ribeiro <franedurib@gmail.com>
2017-03-12 21:55:14 -0300

    protótipo da barra de ferramentas adicionado

--- 28309_Aula_3/pacotes_28309/pacotes_28309/view/TelaApp.java
+++ 28309_Aula_3/pacotes_28309/pacotes_28309/view/TelaApp.java
@@ -4,25 +4,20 @@ import java.awt.*;
 import javax.swing.*;
 import pacotes_28309.control.*;
 
-/**
- * Classe Tela: Pricipal view da aplicação.
- * 
- * @author Francis Ribeiro
- */
 public class TelaApp extends JFrame {
-	public JButton btnCarregarImagem;
+	private JButton btnAbrirImagem, btnZoomMais, btnZoomMenos, btnGirarEsquerda, btnGirarDireita;
+	private JButton btnEspelhar, btnFrente, btnTras, btnCima, btnBaixo;
 
 	/**
 	 * Contrutor da classe. Define as configurações da janela principal da
 	 * aplicacação.
 	 * 
-	 * @param appControl
-	 *            Controle principal da aplicação.
+	 * @param appControl Controle principal da aplicação.
 	 */
 	public TelaApp(AppControl appControl) {
 
 		// Setando as configurações da janela.
-		this.setTitle("App Chassi");
+		this.setTitle("Transformações Geométricas");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setLayout(new BorderLayout());
@@ -39,10 +34,8 @@ public class TelaApp extends JFrame {
 
 	/**
 	 * 
-	 * @param frame
-	 *            JFrame da classe {@link #Tela(AppControl)}.
-	 * @param appControl
-	 *            Controle principal da aplicação.
+	 * @param frame JFrame da classe {@link #Tela(AppControl)}.
+	 * @param appControl Controle principal da aplicação.
 	 */
 	protected void toolBar(JFrame frame, AppControl appControl) {
 		JToolBar toolBar;
@@ -52,13 +45,58 @@ public class TelaApp extends JFrame {
 		toolBar.setRollover(true);
 
 		// Cria os botões.
-		btnCarregarImagem = new JButton("Botão");
+		btnAbrirImagem = new JButton("Abrir Imagem");
+		btnZoomMais = new JButton("Zoom +");
+		btnZoomMenos = new JButton("Zoom -");
+		btnGirarEsquerda = new JButton("<-- Girar");
+		btnGirarDireita = new JButton("Girar -->");
+		btnEspelhar = new JButton("Espelhar");
+		btnFrente = new JButton("Frente");
+		btnTras = new JButton("Trás");
+		btnCima = new JButton("Cima");
+		btnBaixo = new JButton("Baixo");
 
 		// Adiciona os listeners.
-		btnCarregarImagem.addActionListener(appControl);
+		btnAbrirImagem.addActionListener(appControl);
+		btnZoomMais.addActionListener(appControl);
+		btnZoomMenos.addActionListener(appControl);
+		btnGirarEsquerda.addActionListener(appControl);
+		btnGirarDireita.addActionListener(appControl);
+		btnEspelhar.addActionListener(appControl);
+		btnFrente.addActionListener(appControl);
+		btnTras.addActionListener(appControl);
+		btnCima.addActionListener(appControl);
+		btnBaixo.addActionListener(appControl);
 
-		toolBar.add(btnCarregarImagem);
+		// Adicionado os botões na barra de ferramentas
+		toolBar.add(btnAbrirImagem);
 		toolBar.addSeparator();
+
+		toolBar.add(btnZoomMais);
+		toolBar.addSeparator();
+
+		toolBar.add(btnZoomMenos);
+		toolBar.addSeparator();
+
+		toolBar.add(btnGirarEsquerda);
+		toolBar.addSeparator();
+
+		toolBar.add(btnGirarDireita);
+		toolBar.addSeparator();
+
+		toolBar.add(btnEspelhar);
+		toolBar.addSeparator();
+
+		toolBar.add(btnFrente);
+		toolBar.addSeparator();
+
+		toolBar.add(btnTras);
+		toolBar.addSeparator();
+
+		toolBar.add(btnCima);
+		toolBar.addSeparator();
+
+		toolBar.add(btnBaixo);
 
 		// Adiciona toolbar ao JFrame.
 		frame.add(toolBar, BorderLayout.NORTH);
