3c327b4e95cee8ecfafcc971edc878738aec76dc
Francis Eduardo Ribeiro <franedurib@gmail.com>
2017-03-15 09:45:59 -0300

    aula 4 - primeiro commit

---
+++ 28309_Aula_4/pacotes_28309/pacotes_28309/control/AppControl.java
@@ -0,0 +1,29 @@
+package pacotes_28309.control;
+
+import java.awt.event.*;
+import pacotes_28309.view.*;
+
+/**
+ * Classe AppControl: Principal controler da aplicação.
+ * 
+ * @author Francis Ribeiro
+ */
+public class AppControl implements ActionListener {
+	
+	private AppView appView;
+
+	/**
+	 * Construtor da classe. Exibe a aplicação.
+	 */
+	public AppControl() {
+		appView = new AppView(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Botão")) {
+			System.out.println("cliquei no Botão");
+		}
+	}
+
+}
---
+++ 28309_Aula_4/pacotes_28309/pacotes_28309/view/AppView.java
@@ -0,0 +1,66 @@
+package pacotes_28309.view;
+
+import java.awt.*;
+import javax.swing.*;
+import pacotes_28309.control.*;
+
+/**
+ * Classe Tela: Pricipal view da aplicação.
+ * 
+ * @author Francis Ribeiro
+ */
+public class AppView extends JFrame {
+	public JButton btnCarregarImagem;
+
+	/**
+	 * Contrutor da classe. Define as configurações da janela principal da
+	 * aplicacação.
+	 * 
+	 * @param appControl
+	 *            Controle principal da aplicação.
+	 */
+	public AppView(AppControl appControl) {
+
+		// Setando as configurações da janela.
+		this.setTitle("App Chassi");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setLayout(new BorderLayout());
+		this.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+
+		// Adicionando a barra de ferramentas ao JFrame.
+		toolBar(this, appControl);
+
+		// Empacotando e exibindo a aplicação.
+		this.pack();
+		this.setVisible(true);
+		this.setResizable(false);
+	}
+
+	/**
+	 * 
+	 * @param frame
+	 *            JFrame da classe {@link #Tela(AppControl)}.
+	 * @param appControl
+	 *            Controle principal da aplicação.
+	 */
+	protected void toolBar(JFrame frame, AppControl appControl) {
+		JToolBar toolBar;
+
+		// Cria a barra de ferramentas.
+		toolBar = new JToolBar();
+		toolBar.setRollover(true);
+
+		// Cria os botões.
+		btnCarregarImagem = new JButton("Botão");
+
+		// Adiciona os listeners.
+		btnCarregarImagem.addActionListener(appControl);
+
+		toolBar.add(btnCarregarImagem);
+		toolBar.addSeparator();
+
+		// Adiciona toolbar ao JFrame.
+		frame.add(toolBar, BorderLayout.NORTH);
+	}
+}
