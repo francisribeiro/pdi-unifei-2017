7bd285f3d917a801fbb7bf4be77dece7a5227a80
Francis Ribeiro <franedurib@gmail.com>
2017-04-07 08:34:23 -0300

    interface básica

--- 28309_Aula_6/pacotes/pacotes_28309/control/AppControl.java
+++ 28309_Aula_6/pacotes/pacotes_28309/control/AppControl.java
@@ -1,5 +1,75 @@
 package pacotes_28309.control;
 
-public class AppControl {
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
 
+import pacotes_28309.view.*;
+
+public class AppControl implements ActionListener {
+
+	private AppView appView;
+	private BufferedImage img;
+
+	/**
+	 * Construtor da classe. Exibe a aplicação.
+	 */
+	public AppControl() {
+		appView = new AppView(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Abrir Imagem
+		if (e.getSource() == appView.btnAbrirImagem) {
+			//appView.plotaImagem(abrirImagem(), appView.left);
+			if (img != null) {
+				appView.habilitarBotoes();
+			}
+		}
+
+		// Equalizar
+		if (e.getSource() == appView.btnRuidos) {
+		
+		}
+
+		// Fechar
+		if (e.getSource() == appView.btnFechar) {
+			System.exit(0);
+		}
+
+	}
+
+	/**
+	 * Método para navegar pela estrutura de diretórios e carregar uma imagem.
+	 * 
+	 */
+	private BufferedImage abrirImagem() {
+		String nomeArqLido = null;
+
+		// Localizando o arquivo
+		JFileChooser arquivo = new JFileChooser();
+		File diretorio = new File("..\\");
+		arquivo.setCurrentDirectory(diretorio);
+		arquivo.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int saida = arquivo.showOpenDialog(arquivo);
+
+		// Fazendo a leitura do arquivo
+		if (saida == JFileChooser.APPROVE_OPTION) {
+			File nomeArq = arquivo.getSelectedFile();
+			nomeArqLido = nomeArq.toString();
+		} else if (saida == JFileChooser.CANCEL_OPTION)
+			return null;
+
+		// Aloca a Imagem carregada
+		try {
+			img = ImageIO.read(new File(nomeArqLido));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return img;
+	}
 }
--- 28309_Aula_6/pacotes/pacotes_28309/view/AppView.java
+++ 28309_Aula_6/pacotes/pacotes_28309/view/AppView.java
@@ -1,5 +1,169 @@
 package pacotes_28309.view;
 
-public class AppView {
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+import pacotes_28309.control.*;
+
+@SuppressWarnings("serial")
+public class AppView extends JFrame {
+	public JButton btnAbrirImagem, btnRuidos, btnFechar;
+	private JPanel container, um, dois, tres, quatro;
+	public JPanel um_top, um_bottom, dois_top, dois_bottom, tres_top, tres_bottom, quatro_top, quatro_bottom;
+
+	/**
+	 * Contrutor da classe. Define as configurações da janela principal da
+	 * aplicacação.
+	 * 
+	 * @param appControl Controle principal da aplicação.
+	 */
+	public AppView(AppControl appControl) {
+
+		// Setando as configurações da janela.
+		this.setTitle("App Equalizar");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setLayout(new BorderLayout());
+		this.setUndecorated(true);
+
+		// Adicionando a barra de ferramentas ao JFrame.
+		toolBar(this, appControl);
+		montaLayout();
+
+		// Empacotando e exibindo a aplicação.
+		this.pack();
+		this.setVisible(true);
+		this.setResizable(false);
+	}
+
+	/**
+	 * Monta o layout de janelas
+	 */
+	private void montaLayout() {
+
+		// Criando os painéis
+		container = new JPanel(new GridLayout(1, 4));
+		um = new JPanel(new GridLayout(2, 1));
+		dois = new JPanel(new GridLayout(2, 1));
+		tres = new JPanel(new GridLayout(2, 1));
+		quatro = new JPanel(new GridLayout(2, 1));
+
+		um_top = new JPanel();
+		um_bottom = new JPanel();
+		dois_top = new JPanel();
+		dois_bottom = new JPanel();
+		tres_top = new JPanel();
+		tres_bottom = new JPanel();
+		quatro_top = new JPanel();
+		quatro_bottom = new JPanel();
+		
+		um.add(um_top);
+		um.add(um_bottom);
+		dois.add(dois_top);
+		dois.add(dois_bottom);
+		tres.add(tres_top);
+		tres.add(tres_bottom);
+		quatro.add(quatro_top);
+		quatro.add(quatro_bottom);
+		
+		um_top.setBackground(Color.blue);
+		um_bottom.setBackground(Color.cyan);
+		dois_top.setBackground(Color.black);
+		dois_bottom.setBackground(Color.yellow);
+		tres_top.setBackground(Color.blue);
+		tres_bottom.setBackground(Color.cyan);
+		quatro_top.setBackground(Color.black);
+		quatro_bottom.setBackground(Color.yellow);
+		
+		container.add(um);
+		container.add(dois);
+		container.add(tres);
+		container.add(quatro);
+		add(container);
+
+	}
+
+	/**
+	 * 
+	 * @param frame JFrame da classe {@link #Tela(AppControl)}.
+	 * @param appControl Controle principal da aplicação.
+	 */
+	protected void toolBar(JFrame frame, AppControl appControl) {
+		JToolBar toolBar;
+
+		// Cria a barra de ferramentas.
+		toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		toolBar.setOrientation(JToolBar.HORIZONTAL);
+
+		// Cria os botões.
+		btnAbrirImagem = new JButton("Abrir Imagem");
+		btnRuidos = new JButton("Ruídos");
+		btnFechar = new JButton("Fechar");
+
+		// Desabilita os botões
+		btnRuidos.setEnabled(false);
+
+		// Adiciona os listeners.
+		btnAbrirImagem.addActionListener(appControl);
+		btnRuidos.addActionListener(appControl);
+		btnFechar.addActionListener(appControl);
+
+		// Adicionado os botões na barra de ferramentas
+		toolBar.add(btnAbrirImagem);
+		toolBar.addSeparator();
+
+		toolBar.add(btnRuidos);
+		toolBar.addSeparator();
+
+		toolBar.add(btnFechar);
+		toolBar.addSeparator();
+
+		// Adiciona toolbar ao JFrame.
+		add(toolBar, BorderLayout.NORTH);
+	}
+
+	/**
+	 * Repinta o canvas para os eventos futuros.
+	 * 
+	 * @param g graficos
+	 */
+	private void limparTela(Graphics g, JPanel panel) {
+		g.clearRect(0, 0, panel.getWidth(), panel.getHeight());
+		g.setColor(Color.GRAY);
+		g.fillRect(0, 0, panel.getWidth(), panel.getHeight());
+	}
+
+	/**
+	 * Método para sobreescrever o método de pintura da tela
+	 * 
+	 * @param g gráficos
+	 * @param img imagem para ser plotada
+	 */
+	public void paintComponent(Graphics g, BufferedImage img, JPanel panel) {
+		int x = (panel.getWidth() - img.getWidth()) / 2;
+		int y = (panel.getHeight() - img.getHeight()) / 2;
+
+		panel.paintComponents(g);
+		limparTela(g, panel);
+		g.drawImage(img, x, y, this);
+	}
+
+	/**
+	 * Plota a imagem na tela
+	 * 
+	 * @param img de entrada
+	 */
+	public void plotaImagem(BufferedImage img, JPanel panel) {
+		if (img != null)
+			paintComponent(panel.getGraphics(), img, panel);
+	}
+
+	/**
+	 * Habilita botão de equalizar a imagem, logo após uma imagem ser aberta.
+	 */
+	public void habilitarBotoes() {
+		btnRuidos.setEnabled(true);
+	}
 
 }
